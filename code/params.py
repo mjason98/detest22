6f799cc353f39314e2c31d51a7fe4d2fd86d4d31
mjason <mjasoncuba@gmail.com>
2022-04-11 22:21:59 -0400

    Added basic parameters

---
+++ code/params.py
@@ -0,0 +1,18 @@
+PARAMETERS = {
+    # Languages used by the system
+    "languages": ["es", "en"],
+
+    # Transformers models matching the language
+    "transformers_by_language":{
+        "es":"",
+        "en":""
+    },
+
+    # Training parameters --------------------------------------------------------
+    
+    # * Learning rate hyperparameter used by default in training process
+    "lr": 0.005,
+
+    # * Number of epochs in the training process
+    "epochs": 15,
+}
---
+++ main.py
@@ -0,0 +1,35 @@
+import sys, argparse, os
+from code.params import PARAMETERS
+
+TRAIN_DATA_PATH = os.path.join("data", "train.csv")
+EVAL_DATA_PATH = ""
+LANGUAGE = "es"
+
+def check_params(arg=None):
+    global TRAIN_DATA_PATH
+    global EVAL_DATA_PATH
+    global LANGUAGE
+
+    parse = argparse.ArgumentParser(description='Deep Model to solve IverLeaf2021 HAHA Task')
+
+    parse.add_argument('-t', dest='train_data', help='Train Data', 
+                       required=False, default=TRAIN_DATA_PATH)
+    parse.add_argument('-e', dest='eval_data', help='Evaluation Data', 
+                       required=False, default=EVAL_DATA_PATH)
+    
+    parse.add_argument('--lang', dest='language', help='The language of the system', 
+                       required=False, default=LANGUAGE, choices=PARAMETERS["languages"])
+
+    parse.add_argument('--seed', dest='my_seed', help='Random Seed', 
+                       required=False, default=1234567)
+
+    returns = parse.parse_args(arg)
+    TRAIN_DATA_PATH = returns.train_data
+    EVAL_DATA_PATH  = returns.eval_data
+    LANGUAGE = returns.language
+
+
+
+if __name__ == "__main__":
+    if check_params(arg=sys.argv[1:]) == 0:
+        exit(0)
